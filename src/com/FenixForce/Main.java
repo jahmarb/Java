e820eaf4277e28de75a112f2eabf96cdeff327c5
jahmarb <jahmarb7@gmail.com>
2021-10-03 18:38:51 -0700

    Update 10/3/2021 18:36

---
+++ src/com/FenixForce/Main.java
@@ -0,0 +1,10 @@
+package com.FenixForce;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        int testNum = 123456789;
+        int displayNum = DescendingOrder.sortDesc(testNum);
+	System.out.println("\nResult in main:"+displayNum);
+    }
+}
